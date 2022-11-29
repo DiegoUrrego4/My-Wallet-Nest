97c6f9c6aa8eb64c4cd3bddb3e32cb93069b9445
Diego Urrego <diego.urrego@sofka.com.co>
2022-11-29 17:46:26 -0500

    Creación de módulos necesarios. Aún sin funcionalidades ni exportaciones.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientModule } from './client/client.module';
+import { AccountModule } from './account/account.module';
+import { AplicationModule } from './app/app.module';
+import { MovementModule } from './movement/movement.module';
 
 @Module({
-  imports: [],
+  imports: [ClientModule, AccountModule, AplicationModule, MovementModule],
   controllers: [AppController],
   providers: [AppService],
 })
