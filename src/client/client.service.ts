68ef02a855fbb4a4bd1e3cc9aaa504ab95d0155d
Diego Urrego <diego.urrego@sofka.com.co>
2022-12-01 12:28:24 -0500

    Inicio de cambios en las entidades de cada modulo.

--- src/client/client.service.ts
+++ src/client/client.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
+import { ClientEntity } from './entities/client.entity';
 
 @Injectable()
 export class ClientService {
+  constructor(
+    @InjectRepository(ClientEntity)
+    private clientRepository: Repository<ClientEntity>,
+  ) {}
+
   createClient(createClientDto: CreateClientDto) {
     return {
       token: 'Este es tu nuevo token',
