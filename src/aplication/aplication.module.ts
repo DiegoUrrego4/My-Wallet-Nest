aee51cfb5eef68a5a4b86eba2df6a1355f70bb54
Diego Urrego <diego.urrego@sofka.com.co>
2022-12-01 21:54:14 -0500

    test iniciales modulo aplication.

--- src/aplication/aplication.module.ts
+++ src/aplication/aplication.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AplicationService } from './aplication.service';
 import { AplicationController } from './aplication.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AplicationEntity } from './entities/aplication.entity';
 
 @Module({
   controllers: [AplicationController],
   providers: [AplicationService],
+  imports: [TypeOrmModule.forFeature([AplicationEntity])],
 })
 export class AplicationModule {}
